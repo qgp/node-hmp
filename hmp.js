e882f3152deb3d4397779e62ab053cb1849f2d30
Jochen Klein <Jochen.Klein@cern.ch>
2020-08-12 23:08:12 +0200

    Update

--- hmp.js
+++ hmp.js
@@ -4,37 +4,40 @@ function HMP(serial) {
   this._serial = serial
   this._busy = false
   this._queue = []
-  var hmp = this;
-
-  console.log("New HMP")
-  //this._serial.write('*IDN?\n')
-  this.send('*IDN?')
-  this.send('SYSTEM:REMOTE')
-  this.send('*IDN?')
-
   var device = this
+
+  this.ask('*IDN?', (arg) => { console.log('updating %s', arg); device._idn = arg })
+  this.cmd('SYSTEM:REMOTE')
+  this.ask('*IDN?', this._idn)
+
   this._serial.on('data', function (data) {
-    console.log('%s -> %s', device._current, data)
+    console.log('%s -> %s (%s)', device._current[0], data, device._current[1])
+    if (device._current[1]) device._current[1](data.toString().trim())
     device.processQueue()
   })
-
-//  this._serial.on('readable', function () {
-//    console.log('Data: %s', this._serial.read())
-//  })
 
   this._serial.on('error', function(err) {
     console.log('Error: ', err.message)
   })
 }
 
-HMP.prototype.send = function(data) {
-  this._queue.push(data)
+HMP.prototype.send = function(data, ref) {
+  this._queue.push([data, ref])
   if (this._busy) return;
   this._busy = true
   this.processQueue()
 }
 
+HMP.prototype.cmd = function(data) {
+  this.send(data)
+}
+
+HMP.prototype.ask = function(data, ref) {
+  this.send(data, ref)
+}
+
 HMP.prototype.processQueue = function() {
+  console.log('%s', this)
   var next = this._queue.shift()
 
   if (!next) {
@@ -43,8 +46,10 @@ HMP.prototype.processQueue = function() {
   }
 
   this._current = next
-  this._serial.write(next + '\n')
-  console.log('Sent %s', next)
+  this._serial.write(next[0] + '\n')
+  console.log('Sent %s (%s)', next[0], next[1])
+  var device = this
+  setTimeout(() => { device.processQueue() }, next[1] == null ? 10 : 500)
 }
 
 dev = new HMP(new SerialPort('/dev/ttyUSB0', { baudRate: 57600 }))
